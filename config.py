54e6067bb7c4308a504a69b0ecc3d2046d1a236f
Graham-Broughton <broughton.graham@gmail.com>
2023-01-22 10:41:21 +0000

    everything works, just b4 predictions

--- config.py
+++ config.py
@@ -3,12 +3,10 @@ from dataclasses import dataclass
 
 @dataclass
 class CFG:
-    BUCKET: str = 'gs://finlandforest'
-    TRAIN_SAT: str = os.path.join(BUCKET, 'train_features')
-    TRAIN_AGBM: str = os.path.join(BUCKET, 'train_agbm')
-    TEST: str = os.path.join(BUCKET, 'test')
+
     SEED: int = 42
-    PATH = 'gcp_bucket/train_features/'
+    BATCH_SIZE: int = 32
+    WORKERS: int = 8
 
     BAND_MAP = {  
         # S2 bands
@@ -38,13 +36,13 @@ class CFG:
         21: 'S2-SWI: Sandardized Water-Level Index, 20m',
         22: 'S1-VV/VH-Asc: Cband-10m',
         23: 'S2-VV/VH-Desc: Cband-10m'
-    }
+}
 
     MONTH_MAP = {
         0: 'Sep', 1: 'Oct', 2: 'Nov', 3: 'Dec',
         4: 'Jan', 5: 'Feb', 6: 'Mar', 7: 'Apr',
         8: 'May', 9: 'Jun', 10: 'Jul', 11: 'Aug'
-    }
+}
 
     CHANNEL_MAP = {
     0: 'S2-B2: Blue-10m',
@@ -64,18 +62,15 @@ class CFG:
     14: 'S1-VH-Desc: Cband-10m',
     15: 'S2-NDVI: (NIR-Red)/(NIR+Red) 10m',
     16: 'S1-VV/VH-Asc: Cband-10m'
-    }
+}
 
-    def __post_init__(self):
-        tmp = os.path.realpath(__file__)
-        not_base_path = True
-        while not_base_path:
-            if os.path.split(tmp)[1] == 'finland_forest':
-                not_base_path = False
-                self.BASE = tmp
-            else:
-                tmp = os.path.split(tmp)[0]
-        self.DATA = os.path.join(self.BASE, 'data')
-        self.SRC = os.path.join(self.BASE, 'src')
-        self.TILE_CSV = os.path.join(self.DATA, 'df_tile_list.csv')
-
+    PLOT_CHANNELS = {
+    0: {'s2_rgb_idxs': [2, 1, 0], 'title': 'RGB'}, # RGB image, special case 
+    1: {'data': 'target', 'title': 'AGBM', 'LogNorm': True}, # AGBM target data, special case 
+    2: {'channel_idx': 6},  # general case of visualizing by channel_index 
+    3: {'channel_idx': 15},
+    4: {'channel_idx': 10},
+    5: {'channel_idx': 12}, 
+    6: {'channel_idx': 16},
+    7: {'s1_rgb_idxs': [11, 12, 16], 'title': 'SAR-RGB: VV,VH,VV/VH'}, # SAR psuedo-RGB, special case 
+}
