b467ca5d465196676c0a9e935818548b666a691a
Graham-Broughton <broughton.graham@gmail.com>
2023-01-26 09:10:22 +0000

    added panoptic

--- config.py
+++ config.py
@@ -1,97 +1,28 @@
-import os
-from dataclasses import dataclass, asdict, field
+from dataclasses import dataclass, field
 
 
 @dataclass
 class CFG:
-    SEED: int = 42
-    BATCH_SIZE: int = 64
-    WORKERS: int = 0
-    EPOCHS: int = 50
-    LR: int = 0.02
-    PREFETCH: int = 2
-    BANDS: list[int] = field(default_factory=lambda: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15, 16, 17, 18, 19, 20, 21])
-    TRAIN_FRAC: float = 0.8
-    MAX_CHIPS: int = None
-    WORKING_DIR: str = '/home/broug/Desktop/DD-Finland-Forests'
-    DIR_TILES: str = f'{WORKING_DIR}/data/train/train_features/'
-    DIR_TARGET: str = f'{WORKING_DIR}/data/train/train_agbm/'
-    DIR_TEST: str = f'{WORKING_DIR}/data/test/test_features/'
-    SAVED_MODELS: str = f'{WORKING_DIR}/trained_models/'
-    TILE_FILE: str = f'{WORKING_DIR}/data/TILE_LIST_BEST_MONTHS.csv'
-    TILE_FILE_TEST: str = f'{WORKING_DIR}/data/TILE_LIST_BEST_TEST_MONTHS.csv'
-    DIR_PREDS: str = f'{WORKING_DIR}/predictions/baseline/'
-
-    def todict(self):
-        return asdict(self)
-
-
-@dataclass
-class CFG2:
-
-    BAND_MAP = {  
-        # S2 bands
-        0: 'S2-B2: Blue-10m',
-        1: 'S2-B3: Green-10m',
-        2: 'S2-B4: Red-10m',
-        3: 'S2-B5: VegRed-704nm-20m',
-        4: 'S2-B6: VegRed-740nm-20m',
-        5: 'S2-B7: VegRed-780nm-20m',
-        6: 'S2-B8: NIR-833nm-10m',
-        7: 'S2-B8A: NarrowNIR-864nm-20m',
-        8: 'S2-B11: SWIR-1610nm-20m',
-        9: 'S2-B12: SWIR-2200nm-20m',
-        10: 'S2-CLP: CloudProb-160m',
-        # S1 bands
-        11: 'S1-VV-Asc: Cband-10m',
-        12: 'S1-VH-Asc: Cband-10m',
-        13: 'S1-VV-Desc: Cband-10m',
-        14: 'S1-VH-Desc: Cband-10m',
-        # Bands derived by transforms 
-        15: 'S2-NDVI: (NIR-Red)/(NIR+Red) 10m',
-        16: 'S1-NDVVVH-Asc: Norm Diff VV & VH, 10m',
-        17: 'S2-NDBI: Difference Built-up Index, 20m',
-        18: 'S2-NDRE: Red Edge Vegetation Index, 20m',
-        19: 'S2-NDSI: Snow Index, 20m',
-        20: 'S2-NDWI: Water Index, 10m',
-        21: 'S2-SWI: Sandardized Water-Level Index, 20m',
-        22: 'S1-VV/VH-Asc: Cband-10m',
-        23: 'S2-VV/VH-Desc: Cband-10m'
-}
-
-    MONTH_MAP = {
-        0: 'Sep', 1: 'Oct', 2: 'Nov', 3: 'Dec',
-        4: 'Jan', 5: 'Feb', 6: 'Mar', 7: 'Apr',
-        8: 'May', 9: 'Jun', 10: 'Jul', 11: 'Aug'
-}
-
-    CHANNEL_MAP = {
-    0: 'S2-B2: Blue-10m',
-    1: 'S2-B3: Green-10m',
-    2: 'S2-B4: Red-10m',
-    3: 'S2-B5: VegRed-704nm-20m',
-    4: 'S2-B6: VegRed-740nm-20m',
-    5: 'S2-B7: VegRed-780nm-20m',
-    6: 'S2-B8: NIR-833nm-10m',
-    7: 'S2-B8A: NarrowNIR-864nm-20m',
-    8: 'S2-B11: SWIR-1610nm-20m',
-    9: 'S2-B12: SWIR-2200nm-20m',
-    10: 'S2-CLP: CloudProb-160m',
-    11: 'S1-VV-Asc: Cband-10m',
-    12: 'S1-VH-Asc: Cband-10m',
-    13: 'S1-VV-Desc: Cband-10m',
-    14: 'S1-VH-Desc: Cband-10m',
-    15: 'S2-NDVI: (NIR-Red)/(NIR+Red) 10m',
-    16: 'S1-VV/VH-Asc: Cband-10m'
-}
-
-    PLOT_CHANNELS = {
-    0: {'s2_rgb_idxs': [2, 1, 0], 'title': 'RGB'}, # RGB image, special case 
-    1: {'data': 'target', 'title': 'AGBM', 'LogNorm': True}, # AGBM target data, special case 
-    2: {'channel_idx': 6},  # general case of visualizing by channel_index 
-    3: {'channel_idx': 15},
-    4: {'channel_idx': 10},
-    5: {'channel_idx': 12}, 
-    6: {'channel_idx': 16},
-    7: {'s1_rgb_idxs': [11, 12, 16], 'title': 'SAR-RGB: VV,VH,VV/VH'}, # SAR psuedo-RGB, special case 
-}
+    encoder_widths: list = field(default_factory=[64, 64, 64, 128])
+    decoder_widths: list = field(default_factory=[32, 32, 64, 128])
+    out_conv: list = field(default_factory=[32, 20])
+    str_conv_k: int = 4
+    str_conv_s: int = 2
+    str_conv_p: int = 1
+    agg_mode: str = "att_group"
+    encoder_norm: str = "group"
+    n_head: int = 16
+    d_model: int = 256
+    d_k: int = 4
+    num_workers: int = 8
+    display_step: int = 100
+    epochs: int = 1
+    batch_size: int = 32
+    lr: float = 0.001
+    fold: int = 1
+    num_classes: int = 1
+    ignore_index: int = -1
+    pad_value: int = 0
+    padding_mode: str = 'reflect'
+    val_every: int = 1
+    val_after: int = 0
---
+++ src/data_loaders/data_loader_v2.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import rasterio
+import src.data_geo.transforms as tf
+import numpy as np
+import os
+from torch.utils.data import Dataset
+import torch
+from glob import glob
+import warnings
+warnings.filterwarnings("ignore", category=rasterio.errors.NotGeoreferencedWarning)
+
+
+class SentinelDataset(Dataset):
+    def __init__(
+            self, 
+            tile_file, 
+            dir_tiles, 
+            dir_target, 
+            metadata,
+            path,
+            max_chips=None, 
+            transform=None, 
+            device='cpu', 
+            is_train=True
+        ):
+        self.metadata = pd.read_csv(metadata)
+        self.path = path
+        self.dir_tiles = dir_tiles
+        self.dir_target = dir_target
+        self.device = device        
+        if tile_file is not None:
+            self.df_tile_list = pd.read_csv(tile_file)[['chipid', 'month']]
+        else:
+            self.df_tile_list = self._make_tile_file()
+        if max_chips:
+            self.df_tile_list = self.df_tile_list[:max_chips]
+        # self.df_tile_list = self.df_tile_list[['chip_id', 'month']].to_records(index=False).tolist()
+        self.transform_s2 = tf.Sentinel2Scale()
+        self.transform_s1 = tf.Sentinel1Scale()
+        self.transform = transform
+        
+    def _make_tile_file(self):
+        if self.dir_target:
+            files = self.metadata[self.metadata['split'] == 'train']
+            filename = 'train_tiles.csv'
+        else:
+            files = self.metadata[self.metadata['split'] == 'test']
+            filename = 'test_tiles.csv'
+
+        files = pd.DataFrame(
+                    files['filename'].apply(
+                        lambda x: str(x).split('.')[0].split("_")[::2]
+                    ).tolist(), columns=['chip_id', 'month']
+                ).drop_duplicates()
+        files['month'] = files['month'].astype(int)
+        return files.sort_values(['chip_id', 'month']).reset_index(drop=True)
+
+    def _load_agbm_tile(self, chipid):
+        target_path = os.path.join(self.dir_target, f'{chipid}_agbm.tif')
+        return self._read_tif_to_tensor(target_path)
+
+    def _read_tif_to_tensor(self, tif_path):
+        with rasterio.open(tif_path) as src:
+            X = torch.tensor(src.read().astype(np.float32),
+                             dtype=torch.float32,
+                             device=self.device,
+                             requires_grad=False,
+                             )
+        return X
+
+    def _load_sentinel_tiles(self, sentinel_type, chipid, month):
+        file_name = f'{chipid}_{sentinel_type}_{str(month).zfill(2)}.tif'
+        tile_path = os.path.join(self.dir_tiles, file_name)
+        return self._read_tif_to_tensor(tile_path)
+
+    def __len__(self):
+        return len(self.df_tile_list)
+
+    def __getitem__(self, index):
+        chipid, month = self.df_tile_list.iloc[index].values
+
+        try:
+            s1_tile = self._load_sentinel_tiles('S1', chipid, month)
+            s1_tile_scaled = self.transform_s1(s1_tile)
+        except Exception as e:
+            # print(f'Data load failure for S1: {chipid} {month}')
+            # print(f'Caused by {e}')
+            s1_tile_scaled = torch.full([4, 256, 256], torch.nan, dtype=torch.float32, requires_grad=False, device=self.device)
+        # Sentinel 2
+        try:
+            s2_tile = self._load_sentinel_tiles('S2', chipid, month)
+            s2_tile_scaled = self.transform_s2(s2_tile)
+        except Exception as e:
+            # print(f'Data load failure for S2: {chipid} {month}')
+            # print(f'Caused by {e}')
+            s2_tile_scaled = torch.full([11, 256, 256], torch.nan, dtype=torch.float32, requires_grad=False, device=self.device)
+
+        tile = torch.cat([s1_tile_scaled, s2_tile_scaled], dim=0)
+
+        if self.dir_target is not None:
+            target_tile = self._load_agbm_tile(chipid)
+        else:
+            target_tile = torch.full([1, 256, 256], torch.nan, dtype=torch.float32, requires_grad=False, device=self.device)
+
+        sample = {'image': tile, 'label': target_tile} # 'image' and 'label' are used by torchgeo
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
